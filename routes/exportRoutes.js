a2347330770c38a3bb2d1ebdb47fa6b58ec5d2eb
FathimatusZN <fathimatus.zn18@gmail.com>
2025-10-18 16:23:42 +0700

    feat(export): add route for export need process registration training function

--- routes/exportRoutes.js
+++ routes/exportRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { exportUsers } = require("../controllers/exportController");
+const {
+  exportUsers,
+  exportRegistrationsNeedProcess,
+} = require("../controllers/exportController");
 
 // GET /api/export - Export user data (all/custom)
 router.get("/users", exportUsers);
+
+// GET /api/export/registrations/needprocess - Export registration training data (status 1-3)
+router.get("/registrations/needprocess", exportRegistrationsNeedProcess);
 
 module.exports = router;
